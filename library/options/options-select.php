8277c4ece57a55e309aa5d08f5523acd4b5994bb
CyberChimps <cyberchimpspro@gmail.com>
2011-09-01 12:51:55 -0700

    first commit

---
+++ library/options/options-select.php
@@ -0,0 +1,21 @@
+<?php
+/* 
+	Options	Select
+	Author: Tyler Cuningham
+	Establishes the theme options select arrays.
+	Copyright (C) 2011 CyberChimps
+	Version 2.0
+	
+*/
+
+$select_featured_images = array(
+'0' => array('value' => 'left','label' => __('Left (default)' )),'1' => array('value' => 'center','label' => __('Center')), '2' => array('value' => 'right','label' => __('Right')),
+
+);
+
+$select_font = array(
+	'0' => array('value' =>'Helvetica Neue','label' => 'Helvetica Neue (default)'),'1' => array('value' =>'Allan','label' => 'Allan'),'2' => array('value' =>'Arial','label' => 'Arial'),'3' => array('value' =>'Cantarell','label' => 'Cantarell'),'4' => array('value' =>'Courier New','label' => 'Courier New'),'5' => array('value' =>'Georgia','label' => 'Georgia'),'6' => array('value' =>'Tahoma','label' => 'Tahoma'),'7' => array('value' =>'Times New Roman','label' => 'Times New Roman'),'8' => array('value' =>'Ubuntu','label' => 'Ubuntu'),
+	
+);
+
+?>
